aa2164c933f26aee18f53900b3738a9a4fc4c3eb
Chibi <chibi.m@gmail.com>
2022-08-17 22:04:37 +0200

    First three tests (need to brush up the deletion test).

--- tests.py
+++ tests.py
@@ -1,69 +1,120 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
-driver.get("https://todomvc.com/examples/angular2/")
-driver.maximize_window()
-driver.implicitly_wait(5)
-# new_todo = driver.find_element(By.XPATH, "/html/body/todo-app/section/header/input")
-new_todo = driver.find_element(By.CLASS_NAME, "new-todo")
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+
+
+def run_chrome():
+    webdriver.Chrome()
+    driver.get("https://todomvc.com/examples/angular2/")
+    driver.implicitly_wait(5)
+
+
+def enter_todo(test_text):
+    new_todo = driver.find_element(By.CLASS_NAME, "new-todo")
+    new_todo.send_keys(test_text)
+    new_todo.send_keys(Keys.ENTER)
+    print("Todo \'" + test_text + "\' was added!")
+
 
 # POSITIVE
 
 # Test 1: user can add record
+print("Test 1: user can add record")
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
 # 4. Press enter
-new_todo.send_keys("Hello, world!", Keys.ENTER)
-# 5. Assert: record is added
-
+enter_todo("Buy some bread and cheese")
+# 5. Check if record is added
+if driver.find_elements(By.XPATH, "/html/body/todo-app/section/section/ul/li/div/label"):
+    print("Test 1 PASSED, todo was added.")
+else:
+    print("Test 1 FAILED, todo wasn't added.")
+# 6. Close browser.
 
 # Test 2: user can delete record
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
 # 4. Press enter
+enter_todo("Buy some milk")
+
+
+# 5. Find x
+
+def click_delete():
+    todo_item = driver.find_element(By.XPATH, "//todo-app//li[last()]")
+    hover = ActionChains(driver).move_to_element(todo_item)
+    hover.perform()
+    delete_button = driver.find_element(By.CLASS_NAME, "destroy")
+    hover = ActionChains(driver).move_to_element(delete_button)
+    hover.perform()
+    delete_button.click()
+
+
+click_delete()
+
 # 6. Press x to delete
+
 # 5. Assert: record is deleted
+if not driver.find_elements(By.XPATH, "//todo-app//li[last()]"):
+    print("Test 2 PASSED, todo was deleted.")
+else:
+    print("Test 2 FAILED, todo wasn't deleted.")
 
 # Test 3: user can mark the record as done
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
 # 4. Press enter
+enter_todo("Wake up early")
+
+
 # 6. Press radiobutton to mark as done
+def mark_done(xpath):
+    toggle_item = driver.find_element(By.XPATH, xpath)
+    toggle_item.click()
+
+mark_done("//todo-app//li[last()]/div/input")
+
 # 5. Assert: record is marked as done
+
+if driver.find_elements(By.CLASS_NAME, "completed"):
+    print("Test 3 PASSED, item was marked as done.")
+else:
+    print("Test 3 FAILED, item was not marked as done.")
 
 # Test 4: double click edits the record
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 6. Double click record
 # 5. Assert: record is in editing mode
 
 # Test 5: "Clear completed" appears
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: "Clear completed" appears
 
 # Test 6: Counter changes
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input valid text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: counter is changed
 
@@ -71,44 +122,51 @@ new_todo.send_keys("Hello, world!", Keys.ENTER)
 
 # Test 1: Attempt submitting without text
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input no text
-new_todo.send_keys("", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: nothing happens, no new records
 
 # Test 2: Attempt submitting long text
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input a lot of text
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: text is added
 
 # Test 3: Special symbols (parametrize)
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input special symbols
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: text is added
 
 # Test 4: Attempt submitting spaces
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input spaces
-new_todo.send_keys("    ", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Assert: text isn't added
 
 # Test 5: Attempt identical item entry
 # 1. Open browser
+run_chrome()
 # 2. Find input field
 # 3. Input "check"
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 4. Press enter
 # 5. Find input field
 # 6. Input "check"
-new_todo.send_keys("Hello, world!", Keys.ENTER)
+enter_todo("Buy some bread and cheese")
 # 7. Press enter
 # 8. Assert: error for same data
+
+# driver.close()
